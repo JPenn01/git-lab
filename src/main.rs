7b503f65cdcec89af608d444a4f3f9ed67d567ba
Coder-Cal-1529 <caleb.kellum@my.tccd.edu>
2026-09-23 15:41:51 +0000

    style(main): changed banner to "Have you ever been so hungry you could eat a horse?"

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,7 @@ mod report;
 mod supplies;
 
 fn main() {
-    println!("Oregon Trail Supply Check");
+    println!("Have you ever been so hungry you could eat a horse?");
 
     let party: u32 = 4;
     let days: u32 = 30;
