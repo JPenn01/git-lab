25debfb1f9328e437dc25ab95aeec600d69fbd2f
JPenn01 <jayden.penn@outlook.com>
2026-09-23 16:11:22 +0000

    chore: added rations per day

--- src/main.rs
+++ src/main.rs
@@ -5,8 +5,9 @@ fn main() {
     println!("Have you ever been so hungry you could eat a horse? Wahhhhhhhhhhhhhh");
     let party: u32 = 4;
     let days: u32 = 30;
+    let rations_per_day: u32 = 3;
 
-    let food = supplies::food_needed(party, days);
+    let food = supplies::food_needed(party, days, rations_per_day);
 
     report::print_report(party, days, food);
 }
--- src/supplies.rs
+++ src/supplies.rs
@@ -1,3 +1,3 @@
-pub fn food_needed(party: u32, days: u32) -> u32 {
-    party * days * 3
+pub fn food_needed(party: u32, days: u32, rations_per_day: u32) -> u32 {
+    party * days * rations_per_day
 }
